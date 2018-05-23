74d1b4cac4a6f9b3772a1b08e246614e1acae52c
Robert J <chew-z@users.noreply.github.com>
2018-05-23 14:05:01 +0200

    refactored background.js

--- background.js
+++ background.js
@@ -3,9 +3,7 @@
 'use strict'
 
 
-var hashArray = [];
-
-function Notify(msg, buff) {
+function sendNotification(msg, buff) {
     let blob = new Blob( [ buff ], { type: "image/jpeg" } );
     let urlCreator = window.URL || window.webkitURL;
     let imageUrl = urlCreator.createObjectURL( blob );
@@ -19,7 +17,7 @@ function Notify(msg, buff) {
 }
 
 
-function Wallpaper(url, hash, message) {
+function setWallpaper(url, hash, message) {
     let buffer = null;
     let xhr = new XMLHttpRequest(); 
     xhr.open("GET", "https://www.bing.com" + url, true); 
@@ -34,7 +32,7 @@ function Wallpaper(url, hash, message) {
                 'filename': 'bing_wallpaper'
             }, () => {
                 chrome.storage.local.set({lastHash: hash});
-                Notify(message, buffer);
+                sendNotification(message, buffer);
             });
         }
     }
@@ -42,8 +40,9 @@ function Wallpaper(url, hash, message) {
 }
 
 
-function Run() {
+function onAlarm() {
     console.log(new Date().toString() + 'Got Alarm! Updating wallpaper ...');
+    // First get JSON describing latest Bing wallpapers.
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8');
     xhr.responseType = 'json';
@@ -51,48 +50,58 @@ function Run() {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
                 let json = this.response;
+                // If everything OK - set newest Bing wallpaper as desktop wallpaper
                 if(json.images[0].url) {
                     let imgURL = json.images[0].url;
                     let hash = json.images[0].hsh;
-                    let copy = json.images[0].copyright;
+                    let copy = json.images[0].copyright; //copyright is more of a description
+                    // lastHash is a hash value of newest Bing wallpaper downloaded so far by extension
                     chrome.storage.local.get({lastHash: 'None'}, (items) => {
                         if(hash != items.lastHash)
-                            Wallpaper(imgURL, hash, copy);
+                            // set newest as desktop wallpaper
+                            setWallpaper(imgURL, hash, copy);
                         else
+                            // or pass if nothing new
                             console.log(new Date().toString() + ' New wallpaper not available');
                     });
                 }
+                // myHashArray stores hashes of wallpapers downloaded so far
                 chrome.storage.sync.get('myHashArray', (obj) => {
+                    let hashArray = [];
                     if(obj.hasOwnProperty('myHashArray')) {
                         hashArray = obj.myHashArray;
                         console.log('Found in storage ' + JSON.stringify(hashArray));
                     }
-                });
-                // download all available wallpapers
-                json.images.forEach( (image) => {
-                    let hash = image.hsh;
-                    if( hashArray.includes(hash) ) 
-                        console.log('Found ' + hash);
-                    else {
-                        hashArray.push(hash);
-                        // console.log('hashArray updated ' + JSON.stringify(hashArray));
-                        let url = image.url;
-                        let filename = 'Media/Pictures/Bing' + url.substring(url.lastIndexOf("/"));
-                        console.log(filename);
-                        chrome.downloads.download({
-                            url: 'https://www.bing.com' + url,
-                            filename: filename,
-                            conflictAction: 'overwrite'
-                        });
-                    }
-                });
-                chrome.storage.sync.set({ myHashArray: hashArray }, () => {
-                    if (chrome.runtime.lastError)
-                        console.log(chrome.runtime.lastError);
-                    else
-                        console.log(new Date().toString() + ' hashArray saved ' + JSON.stringify(hashArray));
+                    // download all available wallpapers that we have not downloaded previously
+                    json.images.forEach( (image) => {
+                        let hash = image.hsh;
+                        if( hashArray.includes(hash) )
+                            // pass
+                            console.log('Found ' + hash);
+                        else {
+                            // add current hash to hashArray
+                            hashArray.push(hash);
+                            // console.log('hashArray updated ' + JSON.stringify(hashArray));
+                            let url = image.url;
+                            let filename = 'Media/Pictures/Bing' + url.substring(url.lastIndexOf("/"));
+                            console.log('Downloading ' + filename);
+                            chrome.downloads.download({
+                                url: 'https://www.bing.com' + url,
+                                filename: filename,
+                                conflictAction: 'overwrite'
+                            });
+                        }
+                    });
+                    // after looping over all available wallpapers update myHashArray in storage
+                    chrome.storage.sync.set({ myHashArray: hashArray }, () => {
+                        if (chrome.runtime.lastError)
+                            console.log(chrome.runtime.lastError);
+                        else
+                            console.log(new Date().toString() + ' hashArray saved ' + JSON.stringify(hashArray));
+                    });
                 });
             } else {
+                // Bing JSON isn't accessible - pass, we will retry on next alarm
                 console.log("Something went wrong. Are you connected to internet?");
             }
         }
@@ -101,13 +110,13 @@ function Run() {
 }
 
 
-chrome.alarms.onAlarm.addListener(Run);
+chrome.alarms.onAlarm.addListener(onAlarm);
 
 
 function start() {
-    // try refreshing wallpaper every hour
+    // try refreshing wallpaper every half an hour
     let alarm = 30;
-    chrome.alarms.create("update", {"delayInMinutes": 1,"periodInMinutes": alarm});
+    chrome.alarms.create("update", {"delayInMinutes": 3,"periodInMinutes": alarm});
     console.log(new Date().toString() + ' background set alarm to ' + alarm + ' minutes');
 }
 
